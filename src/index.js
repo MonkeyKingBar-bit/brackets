9312ea569cfcc1aa11edf4f028d7ebf6684f92d0
Catherine Veremeychik <kateiiverem@gmail.com>
2020-10-07 00:57:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let	stack = []
+  
+  for (let i = 0; i < str.length; i++){
+  	let isOpen = false
+  	let isClose = false
+  	let isConch = false
+    let cb = 0
+    for(let j  = 0; j < bracketsConfig.length; j++){
+    	if(str[i] == bracketsConfig[j][0]) {
+      	
+      	isOpen = true
+        cb = j
+      }  
+      if(str[i] == bracketsConfig[j][1]) {
+      
+      	isClose = true
+        cb = j
+      }      
+   	}
+    
+    if (isOpen && isClose){ 
+   
+    	isOpen = false
+      isClose = false
+      isConch = true
+    } 
+    if (isConch){
+    	
+    	if(stack.length == 0 ) stack.push(bracketsConfig[cb][0])   
+      else{
+      	if (stack[stack.length -1 ] == bracketsConfig[cb][0]) stack.pop()
+      	else stack.push(bracketsConfig[cb][0])
+      }
+    }      
+    
+    if(isOpen) stack.push(bracketsConfig[cb][0])
+    if(isClose){
+    	
+    	if(stack.length == 0) return false
+      else {
+      	if (stack[stack.length - 1] == bracketsConfig[cb][0]) stack.pop()
+        else return false
+      }
+    }
+    
+  }	
+ if (stack.length > 0) return false
+ else return true
+  
+
 }
